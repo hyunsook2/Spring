697fcedabaede55583dcae4a9c3a7e590b7719f8
hyunsook2 <127472011+hyunsook2@users.noreply.github.com>
2023-04-27 12:16:44 +0900

    Add files via upload
    0426 spring

---
+++ P16Service/src/main/java/com/study/springboot/P16Service.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P16Service {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P16Service.class, args);
+	}
+
+}
