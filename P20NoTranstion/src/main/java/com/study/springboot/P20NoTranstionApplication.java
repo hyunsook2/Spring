87cf94c780997865c3df5cd62a97e3a36bb7650c
hyunsook2 <127472011+hyunsook2@users.noreply.github.com>
2023-04-28 16:02:55 +0900

    Add files via upload
    spring 0428

---
+++ P20NoTranstion/src/main/java/com/study/springboot/P20NoTranstionApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P20NoTranstionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P20NoTranstionApplication.class, args);
+	}
+
+}
