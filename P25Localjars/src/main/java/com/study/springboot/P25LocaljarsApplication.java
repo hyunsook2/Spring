b6d16de6c620b1f6eb0bfe60f7c27e52dd4c4074
hyunsook2 <127472011+hyunsook2@users.noreply.github.com>
2023-05-02 09:37:36 +0900

    Add files via upload

---
+++ P25Localjars/src/main/java/com/study/springboot/P25LocaljarsApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P25LocaljarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P25LocaljarsApplication.class, args);
+	}
+
+}
