628712941c9e612210c9f92eca6267d07806739b
hyunsook2 <127472011+hyunsook2@users.noreply.github.com>
2023-05-03 14:34:43 +0900

    Add files via upload

--- shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value="/thymeleaf")
@@ -29,4 +31,50 @@ public class ThymeleafExController {
         model.addAttribute("itemDto", itemDto);
         return "thymeleafEx/thymeleafEx02";
     }
+
+    @GetMapping(value = "/ex03")
+    public String thymeleafExam03(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemNm("더미 상품" + i);
+            itemDto.setItemDetail("더미 상품 정보" + i);
+            itemDto.setPrice(1000 * i);
+            itemDto.setRegTime(LocalDateTime.now());
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList",itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
+    }
+    @GetMapping(value = "/ex04")
+    public String thymeleafExam04(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemNm("더미 상품" + i);
+            itemDto.setItemDetail("더미 상품 정보" + i);
+            itemDto.setPrice(1000 * i);
+            itemDto.setRegTime(LocalDateTime.now());
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList",itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
+    @GetMapping(value = "/ex05")
+    public String thymeleafExam05() {
+        return "thymeleafEx/thymeleafEx05";
+    }
+
+    @GetMapping(value = "/ex06")
+    public String thymeleafExam06(String param1,String param2,Model model) {
+        model.addAttribute("param1",param1);
+        model.addAttribute("param2",param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
+
+    @GetMapping(value = "/ex07")
+    public String thymeleafExam07() {
+        return "thymeleafEx/thymeleafEx07";
+    }
 }
+
