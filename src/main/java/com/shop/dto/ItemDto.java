3dbfa5d1b2c8904e6ef44bc371abc3cc49b4d550
hyunsook2 <127472011+hyunsook2@users.noreply.github.com>
2023-05-04 18:26:20 +0900

    Add files via upload
    spirng 오후수업

---
+++ src/main/java/com/shop/dto/ItemDto.java
@@ -0,0 +1,18 @@
+package com.shop.dto;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ItemDto {
+        private Long id;
+        private String itemNm;
+        private int price;
+        private String itemDetail;
+        private String sellStatusCd;
+        private LocalDateTime regTime;
+        private LocalDateTime updateTime;
+
+    }
